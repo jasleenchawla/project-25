b7890fc8f3041fd98d68d0f97687802883ddf2ea
jasleenchawla <69804595+jasleenchawla@users.noreply.github.com>
2020-09-16 20:37:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var paper1, dustbin;
+var paper1, dustbin,ground;
 
 function preload()
 {
@@ -19,8 +19,8 @@ function setup() {
 
 	//Create the Bodies Here.
 	paper1 = new PaperClass(200,350,60)
-	dustbin = new DustbinClass(100,400,40,50);
-
+	dustbin = new DustbinClass(400,600,100,100);
+    ground = createSprite(400,680,800,20);
 	Engine.run(engine);
   
 }
@@ -28,11 +28,17 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(255,255,255);
+  background(0);
   paper1.display()
   dustbin.display()
   drawSprites();
  
+}
+
+function keyPressed(){
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:85,y:-80})
+	}
 }
 
 
